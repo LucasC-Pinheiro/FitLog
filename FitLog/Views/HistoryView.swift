b9b87e6ee21e85ab8b07788a485ac49948d2fd56
Lucas Pinheiro <pinheirotj69@gmail.com>
2026-05-14 20:45:59 -0300

    feat: add HistoryView and ProgressView with Swift Charts
    - Implement HistoryView with workout list sorted by date
    - Implement ProgressView with exercise progress line chart
    - Add weekly volume bar chart using Swift Charts
    - Connect all views to TabView

---
+++ FitLog/Views/HistoryView.swift
@@ -0,0 +1,8 @@
+//
+//  HistoryView.swift
+//  FitLog
+//
+//  Created by Lucas Chaves Pinheiro on 14/05/26.
+//
+
+import Foundation
---
+++ FitLog/Views/ProgressView.swift
@@ -0,0 +1,8 @@
+//
+//  ProgressView.swift
+//  FitLog
+//
+//  Created by Lucas Chaves Pinheiro on 14/05/26.
+//
+
+import Foundation
